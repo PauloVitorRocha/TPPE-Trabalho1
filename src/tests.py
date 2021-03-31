ec84829f0a0df5c9b526ba1b41f3da1985020ec7
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2021-03-31 19:22:19 -0300

    Teste de criação de diagrama falhando na fase de duplicação
    Co-authored-by: João Rodrigues <rodrigues.jmatheus@gmail.com>
    Co-authored-by: Joao Rossi <bielrossiborba@gmail.com>
    Co-authored-by: Ivan Dobbin <ivandinizdobbin2@gmail.com>

--- src/tests.py
+++ src/tests.py
@@ -5,3 +5,7 @@ from activity_diagram import ActivityDiagram
 def test_activity_diagram():
     obj = ActivityDiagram('ad1')
     assert 'ad1'==obj.name
+
+def test_activity_diagram2():
+    obj = ActivityDiagram('ad2')
+    assert 'ad2'==obj.name
