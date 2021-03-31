f5c7ac9607bf65c22b514577d60a3f87cf2bd3ab
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2021-03-31 19:36:24 -0300

    Teste de criação de nó inicial falhando na fase de duplicação
    Co-authored-by: João Rodrigues <rodrigues.jmatheus@gmail.com>
    Co-authored-by: Joao Rossi <bielrossiborba@gmail.com>
    Co-authored-by: Ivan Dobbin <ivandinizdobbin2@gmail.com>

--- src/tests.py
+++ src/tests.py
@@ -15,7 +15,15 @@ def test_activity_diagram3():
     obj = ActivityDiagram('ad3')
     assert 'ad3' == obj.name
 
+
 def test_create_initial_node():
     obj = ActivityDiagram('ad1')
     obj.create_initial_node('n1')
     assert 'n1'==obj.elements.start_node
+
+
+def test_create_initial_node2():
+    obj = ActivityDiagram('ad2')
+    obj.create_initial_node('n2')
+    assert 'n2'==obj.elements.start_node
+
