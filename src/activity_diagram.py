2b6b63b65bc8725852ee320f9185c6cff3d83484
Matheus Rodrigues <rodrigues.jmatheus@gmail.com>
2021-04-02 21:05:02 -0300

    Refatorando o código (activity_diagram.py) para aplicação de testes nas funções do Nó de Decisão
    Co-authored-by: Joao Rossi <bielrossiborba@gmail.com>
    Co-authored-by: Ivan Dobbin <ivandinizdobbin2@gmail.com>
    Co-authored-by: Paulo Vitor <paulovitorrocha.unb@gmail.com>

--- src/activity_diagram.py
+++ src/activity_diagram.py
@@ -1,6 +1,5 @@
 from elements import ActivityElements
 from transitions import ActivityTransitions
-import os
 
 
 class ActivityDiagram():
@@ -12,25 +11,23 @@ class ActivityDiagram():
 
     def create_initial_node(self, name):
         start_node = name
-
         self.elements = ActivityElements(start_node)
 
     def create_transitions(self, name, prob):
         transition = ActivityTransitions(name, prob)
         self.transitions.append(transition)
+#
+#     def create_xml(self):
+#         f = open("diagram.xml", "w")
+#         f.write("<ActivityDiagram name=\"{}\">\n".format(self.name))
 
-    def create_xml(self):
-        f = open("diagram.xml", "w")
-        f.write("<ActivityDiagram name=\"{}\">\n".format(self.name))
-        self.elements.create_elements_xml(f)
-        f.write("\t<ActivityDiagramTransitions>\n")
-        for transition in self.transitions:
-            transition.transition_to_xml(f, False)
-        f.write("\t</ActivityDiagramTransitions>\n")
-        f.write("</ActivityDiagram>")
-        f.close()
-            
-        
-            
-        
+#         self.elements.create_elements_xml(f)
 
+#         f.write("\t<ActivityDiagramTransitions>\n")
+
+#         for transition in self.transitions:
+#             transition.transition_to_xml(f, False)
+
+#         f.write("\t</ActivityDiagramTransitions>\n")
+#         f.write("</ActivityDiagram>")
+#         f.close()
